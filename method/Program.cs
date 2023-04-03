72e90fee88fd571e63991f5a81653c4b9e5e52cd
zidansh <zidansh19@gmail.com>
2023-04-03 14:26:06 +0700

    method

---
+++ method/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string readData = Console.ReadLine();
+        Greetings(readData);
+    }
+
+    public static void Greetings(string name)
+    {
+        Console.WriteLine($"Hello {name} i'm ChatBot nice to meet you");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(true);
+        Console.WriteLine("GoodBye, have a nice day");
+    }
+}
