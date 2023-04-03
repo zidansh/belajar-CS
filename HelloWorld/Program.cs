4df8352fdf52d0e865fa1d403c01082c52cf6bf0
zidansh <zidansh19@gmail.com>
2023-04-03 13:39:12 +0700

    helloworld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HelloWorld");
+    }
+}
